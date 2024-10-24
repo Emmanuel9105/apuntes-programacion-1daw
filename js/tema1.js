602129f29809f854dd799f40af333b3dacc1b6a9
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-10-24 11:16:24 +0200

    U - Apuntes de variables JS

--- js/tema1.js
+++ js/tema1.js
@@ -5,8 +5,9 @@
     de varias lineas
 */
 
+// Declaración antigua que no hace caso al scope (ámbito) de trabajo.
 var variableEnDesuso = 'Declaración de variable en desuso'
-
+// Declaración moderna que tiene en cuenta el scope (ámbito) de trabajo.
 let variableEnUso = 'Declaración de variable en uso'
-
+// Declaración moderna e inmutable que tiene en cuenta el scope (ámbito) de trabajo.
 const CONSTANTE_EN_USO = 'Declaración de constante en uso'
