9d171cf4298b2194ea221801af2a60dc49de42f9
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-10-24 11:00:12 +0200

    A - Variables en JS

--- js/tema1.js
+++ js/tema1.js
@@ -1,5 +1,12 @@
 // Esto es un comentario de una linea
+
 /*
     Esto es un comentario
     de varias lineas
 */
+
+var variableEnDesuso = 'Declaración en desuso'
+
+let variableEnUso = 'Declaración de variable en uso'
+
+const CONSTANTE_EN_USO = 'Declaración de constante en uso'
