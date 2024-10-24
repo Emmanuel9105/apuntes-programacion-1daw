3d0b004a161c55d097ba67082053f42f3fa32d93
Emmanuel <emmanuelcastromartos@gmail.com>
2024-10-24 11:03:17 +0200

    A - variables en JS

--- js/tema1.js
+++ js/tema1.js
@@ -3,3 +3,7 @@
     Esto es un comentario
     de varias lineas
 */
+
+let variableEnUso = 'Declaracion de variable'
+const CONSTANTE_EN_USO = 'Declaracion de constante en uso'
+var variableEnDesuso = 'Declaracion de variable en desuso' // Se utilizaba antes de que existiera el let
