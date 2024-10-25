de5946b090d49c4e71a9298694c557bbd08117f1
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-10-25 11:23:45 +0200

    A - Introducir texto por consola

--- python/tema1.py
+++ python/tema1.py
@@ -7,3 +7,10 @@
 
 # Imprimir información por consola
 print('Esto se ve por consola')
+
+# Introducir información por consola
+# Los datos siempre se envían como texto
+# ! input('Introduce un texto:')
+# Para poder trabajar con los datos hace falta guardarlos en una variable
+# ! text1 = input('Introduce un texto:')
+# ! print(text1)
