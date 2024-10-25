37fc2bc7ca3122f7480258660c8f0b77be8b55d6
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-10-25 12:12:05 +0200

    B - Excepciones Py

--- python/tema1.py
+++ python/tema1.py
@@ -182,9 +182,13 @@ print(resta(30, 8))
 
 try:
   # Código que puede causar excepciones
+  print('Try') # Se muestra por consola
 except:
   # Código para manejar la excepción
+  print('Except') # Se muestra por consola
 else:
   # Código que se ejecuta si no hay excepciones (opcional)
+  print('Else') # No se muestra por consola
 finally:
   # Código que siempre se ejecuta, haya o no una excepción (opcional)
+  print('Finally') # Se muestra por consola
